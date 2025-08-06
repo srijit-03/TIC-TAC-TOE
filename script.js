047ae8d906d84702e12aee0b73ef842c4c2a8192
srijit-03 <srijitdas706@gmail.com>
2025-08-07 01:14:29 +0530

    Add some new features

--- script.js
+++ script.js
@@ -19,63 +19,82 @@ const winPatterns = [
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("box was click");
-     if(turnO) {
-        box.innerText = "O";
-        turnO = false;
-     } else {
-        box.innerText = "X";
-        turnO = true;
-     }
-     box.disabled = true;
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
 
-     checkWin();
+        checkWin();
+        checkDraw();
     });
 });
 
 const resetGame = () => {
-trueO = true;
-enableBoxes();
-msgContainer.classList.add("hide");
-newmagic.classList.remove("hide");
+    trueO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    newmagic.classList.remove("hide");
 };
 
 
 const disableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = true;
     }
 };
 
 const enableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
 };
 
 const showWin = (winner) => {
-  msg.innerText = `Congratulations, Winner is ${winner}`;
-  msgContainer.classList.remove("hide");
-  newmagic.classList.add("hide");
-  disableBoxes();
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    newmagic.classList.add("hide");
+    disableBoxes();
 };
 
 
 const checkWin = () => {
     for (let pattern of winPatterns) {
-     
-       let pos1Val =  boxes[pattern[0]].innerText;
-       let pos2Val =  boxes[pattern[1]].innerText;
-       let pos3Val =  boxes[pattern[2]].innerText;
 
-       if(pos1Val != "" && pos2Val != "" && pos3Val != "" ) {
-        if (pos1Val === pos2Val && pos2Val === pos3Val) {
-            showWin(pos1Val);
-        } 
-       }
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWin(pos1Val);
+                return;
+            }
+        }
     }
 };
+
+
+const checkDraw = () => {
+    let allFilled = true; 
+
+    boxes.forEach((box) => {
+        if (box.innerText === "") {
+            allFilled = false;
+        }
+    });
+
+    if (allFilled) {
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+        newmagic.classList.add("hide");
+    }
+};
+
 
 newGame.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
